ae3ee9eff3821457406498388f1508cd9b67e991
YashwanthYarala <46911439+YashwanthYarala@users.noreply.github.com>
2019-06-01 20:46:09 +0530

    Update task2.py

--- task2/task2.py
+++ task2/task2.py
@@ -1,4 +1,4 @@
 file=str(input("enter  your file name with extension:"))
 r=(file.partition("."))
-filename, separator,extension= file.partition(".")
+filename, separator,extension= r
 print("the extension of the file is:",extension)
