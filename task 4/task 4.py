5887603434cfcabb090d9190cb39f788733480f2
Yashwanth Reddy Yarala <46911439+YashwanthYarala@users.noreply.github.com>
2019-06-07 20:43:22 +0530

    Delete task 4.py

--- task 4/task 4.py
+++
@@ -1,8 +0,0 @@
- # the below python code is to take inputs from the user and reprint them in upper and lower case
-
-name1 = input("enter your string:")
-
-name2 = name1.upper()
-name3 = name1.lower()
-print("your string in UPPER case is :",name2)
-print("your string in LOWER case is :",name3)
