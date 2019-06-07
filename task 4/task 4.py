b687c53d105bba3da60d47c728cbc47bd8e90bf2
Yashwanth Reddy Yarala <46911439+YashwanthYarala@users.noreply.github.com>
2019-06-07 20:35:16 +0530

    Add files via upload
    Task 4
    its about to take input from user and reprint it in both lower and upper case

---
+++ task 4/task 4.py
@@ -0,0 +1,8 @@
+ # the below python code is to take inputs from the user and reprint them in upper and lower case
+
+name1 = input("enter your string:")
+
+name2 = name1.upper()
+name3 = name1.lower()
+print("your string in UPPER case is :",name2)
+print("your string in LOWER case is :",name3)
